4cd989ef307112b16f4f790bc04b508c6a32e846
Дамјан Георгиевски <gdamjan@gmail.com>
2024-12-09 23:06:14 +0100

    remove podman/docker image after creating the initrd

--- build-initramfs.sh
+++ build-initramfs.sh
@@ -6,4 +6,4 @@ CTR_ID=`podman create initramfs.img`
 podman export $CTR_ID | bsdtar -cf - --format newc @- | lz4c -l > run/initramfs.img.lz4
 
 podman rm $CTR_ID
-#podman rmi initramfs.img
+podman rmi initramfs.img
